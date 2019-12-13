9720c013f01561a6c7c1314f0ab0986c02864537
Bernard Brenyah <pimpfada@gmail.com>
2019-12-13 14:23:00 +0100

    refactored scraper and initiate Dockerfile

--- scraper.py
+++ scraper.py
@@ -13,170 +13,181 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 
-# Specify webdriver options
-options = webdriver.ChromeOptions()
-options.add_argument('headless')  # set to headerless windows
-options.add_argument('window-size=1200x600')  # set the window size
 
-# Initiate headerless scraping
-driver = webdriver.Chrome(executable_path = os.getcwd() + '/linux-drivers' + '/chromedriver',
-                         options=options)
+def scrape_data(driver, categories, arxiv_identifier):
+    """
+    """
 
-main_categories = [
-    'Economics', 'Quantitative Biology', 'Quantitative Finance',
-    'Statistics', 'Electrical Engineering', 'Mathematics',
-    'Computer Science', 'Physics', 'Astrophysics'
-]
+    # Initiate master dataframe
+    main_df = pd.DataFrame()
 
-arxiv_names = [
-    'econ', 'q-bio', 'q-fin',
-    'stat', 'eess', 'math',
-    'cs', 'physics', 'astro-ph'
-]
+    for cat, link_name in tqdm(zip(main_categories, arxiv_names)):
 
+        url = f'https://arxiv.org/list/{link_name}/recent'
 
-"""
-main_categories = ['Economics']
-arxiv_names = ['econ']
-"""
+        driver.get(url)
 
-# Initiate master dataframe
-main_df = pd.DataFrame()
+        try:
+            # Wait until the 'all' link is accessible, get this link and click it
+            all_link = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="dlpage"]/small[2]/a[3]'))
+            )
+            all_link.click()
 
-for cat, link_name in tqdm(zip(main_categories, arxiv_names)):
-
-    url = f'https://arxiv.org/list/{link_name}/recent'
-
-    driver.get(url)
-
-    try:
-        # Wait until the 'all' link is accessible, get this link and click it
-        all_link = WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="dlpage"]/small[2]/a[3]'))
-        )
-        all_link.click()
-
-    except TimeoutException:
-        # Subjects with no all click have all their data ready to be scraped
-        pass
+        except TimeoutException:
+            # Subjects with no all click have all their data ready to be scraped
+            pass
 
 
-    # Get the html for the current url
-    time.sleep(2)
-    html = driver.page_source
+        # Get the html for the current url
+        time.sleep(2)
+        html = driver.page_source
 
-    # Parse the html with BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
-    time.sleep(2)
-    # Find the main containers
-    all_dl = soup.find_all('dl')
+        # Parse the html with BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
+        time.sleep(2)
+        # Find the main containers
+        all_dl = soup.find_all('dl')
 
-    for dl in all_dl:
+        for dl in all_dl:
 
-        # Initiate empty list to contain metadata
-        all_titles = []
-        abstract_links = []
-        download_links = []
-        abstract_data = []
-        authors_data = []
-        submission_data = []
-        subjects_data = []
+            # Initiate empty list to contain metadata
+            all_titles = []
+            abstract_links = []
+            download_links = []
+            abstract_data = []
+            authors_data = []
+            submission_data = []
+            subjects_data = []
 
-        # Titles
-        for x in dl.find_all('dd'):
-            # list of all titles
-            titles = [x.text.replace('Title: ', '').replace('\n', '') for x in x.find_all('div', {'class': 'list-title mathjax'})]
+            # Titles
+            for x in dl.find_all('dd'):
+                # list of all titles
+                titles = [x.text.replace('Title: ', '').replace('\n', '') for x in x.find_all('div', {'class': 'list-title mathjax'})]
 
-            # Append titles to all titles list
-            for t in titles:
-                all_titles.append(t)
+                # Append titles to all titles list
+                for t in titles:
+                    all_titles.append(t)
 
-        # Links for abstract, pdf
-        for x in dl.find_all('dt'):
+            # Links for abstract, pdf
+            for x in dl.find_all('dt'):
 
-            all_article_links = x.find_all('a', href=True)
-            link_list = ['https://arxiv.org' + link['href'] for link in all_article_links][0:2]
+                all_article_links = x.find_all('a', href=True)
+                link_list = ['https://arxiv.org' + link['href'] for link in all_article_links][0:2]
 
-            # Append abstract url to abstract links
-            abstract_url = link_list[0]
-            abstract_links.append(abstract_url)
+                # Append abstract url to abstract links
+                abstract_url = link_list[0]
+                abstract_links.append(abstract_url)
 
-            # Append download url to abstract link list
-            download_url = link_list[1]
-            download_links.append(download_url)
+                # Append download url to abstract link list
+                download_url = link_list[1]
+                download_links.append(download_url)
 
-        # Subjects
-        for x in dl.find_all('div', {'class': 'list-subjects'}):
-            subjects = x.text.strip().replace('Subjects: ', '')
-            subjects_data.append(subjects)
+            # Subjects
+            for x in dl.find_all('div', {'class': 'list-subjects'}):
+                subjects = x.text.strip().replace('Subjects: ', '')
+                subjects_data.append(subjects)
 
-        # Scrape the abstract meta-data
-        for link in abstract_links:
+            # Scrape the abstract meta-data
+            for link in abstract_links:
 
-            try:
+                try:
 
-                driver.get(link)
+                    driver.get(link)
 
-                # Abstract text
-                abstract_block = WebDriverWait(driver, 60).until(
-                    EC.presence_of_element_located((By.XPATH, '//*[@id="abs"]/blockquote'))
-                )
-                abstract_text = abstract_block.text
-                abstract_text = abstract_text.replace('Abstract:  ', '')
+                    # Abstract text
+                    abstract_block = WebDriverWait(driver, 60).until(
+                        EC.presence_of_element_located((By.XPATH, '//*[@id="abs"]/blockquote'))
+                    )
+                    abstract_text = abstract_block.text
+                    abstract_text = abstract_text.replace('Abstract:  ', '')
 
-                # Authors text
-                WebDriverWait(driver, 60).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '#abs > div.authors'))
-                )
+                    # Authors text
+                    WebDriverWait(driver, 60).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, '#abs > div.authors'))
+                    )
 
-                authors_text = driver.find_element_by_css_selector('#abs > div.authors').text
+                    authors_text = driver.find_element_by_css_selector('#abs > div.authors').text
 
-                # Submission date text
-                WebDriverWait(driver, 60).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '#abs > div.dateline'))
-                )
+                    # Submission date text
+                    WebDriverWait(driver, 60).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, '#abs > div.dateline'))
+                    )
 
-                submission_date_text = driver.find_element_by_css_selector('#abs > div.dateline').text
-
-
-            except Exception as e:
-
-                # Set authors, abstract and submission info to NaN if scraping fails
-                authors_text = np.NaN
-                abstract_text = np.NaN
-                submission_date_text = np.NaN
-
-            # Append metadata info to the main data lists
-            abstract_data.append(abstract_text)
-            authors_data.append(authors_text)
-            submission_data.append(submission_date_text)
-
-        # Convert meta-data into a dataframe
-        df = pd.DataFrame({'title': all_titles,
-                           'download_url': download_links,
-                           'abstract_link': abstract_links,
-                           'abstract_text': abstract_data,
-                           'authors': authors_data,
-                           'submission_date': submission_data,
-                           'subjects': subjects_data})
-
-        # Tag the current subject
-        df['subject_tag'] = cat
-
-        # Append the subject dataframe to the main dataframe
-        main_df = main_df.append(df)
-
-    time.sleep(3)
-
-# Reset index and export data
-main_df = main_df.reset_index(drop=True)
-main_df.to_csv('data/test.csv', index=False)
-main_df.to_excel('data/test.xlsx', index=False)
-
-# Push scraped data to db
-with sqlite3.connect('data/arxiv.sqlite') as conn:
-    main_df.to_sql('raw_data', if_exists='replace', con=conn, index=False)
+                    submission_date_text = driver.find_element_by_css_selector('#abs > div.dateline').text
 
 
-# Exit application
-driver.quit()
+                except Exception as e:
+                    print(e)
+                    # Set authors, abstract and submission info to NaN if scraping fails
+                    authors_text = np.NaN
+                    abstract_text = np.NaN
+                    submission_date_text = np.NaN
+
+                # Append metadata info to the main data lists
+                abstract_data.append(abstract_text)
+                authors_data.append(authors_text)
+                submission_data.append(submission_date_text)
+
+            # Convert meta-data into a dataframe
+            df = pd.DataFrame({'title': all_titles,
+                            'download_url': download_links,
+                            'abstract_link': abstract_links,
+                            'abstract_text': abstract_data,
+                            'authors': authors_data,
+                            'submission_date': submission_data,
+                            'subjects': subjects_data})
+
+            # Tag the current subject
+            df['subject_tag'] = cat
+
+            # Append the subject dataframe to the main dataframe
+            main_df = main_df.append(df)
+
+        time.sleep(3)
+
+    # Reset index and export data
+    main_df = main_df.reset_index(drop=True)
+    main_df.to_csv('data/test.csv', index=False)
+    main_df.to_excel('data/test.xlsx', index=False)
+
+    # Push scraped data to db
+    with sqlite3.connect('data/arxiv.sqlite') as conn:
+        main_df.to_sql('raw_data', if_exists='replace', con=conn, index=False)
+
+
+    # Exit application
+    driver.quit()
+
+    print('Done scraping all the data')
+
+
+if __name__ == "__main__":
+
+    # Specify webdriver options
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')  # set to headerless windows
+    options.add_argument('window-size=1200x600')  # set the window size
+
+    # Initiate headerless scraping
+    driver = webdriver.Chrome(executable_path = os.getcwd() + '/linux-drivers' + '/chromedriver',
+                            options=options)
+
+    main_categories = [
+        'Economics', 'Quantitative Biology', 'Quantitative Finance',
+        'Statistics', 'Electrical Engineering', 'Mathematics',
+        'Computer Science', 'Physics', 'Astrophysics'
+    ]
+
+    arxiv_names = [
+        'econ', 'q-bio', 'q-fin',
+        'stat', 'eess', 'math',
+        'cs', 'physics', 'astro-ph'
+    ]
+
+    """ # Only used for testing
+    main_categories = ['Economics']
+    arxiv_names = ['econ']
+    """
+
+    scrape_data(driver = driver, categories = main_categories, arxiv_identifier = arxiv_names)
